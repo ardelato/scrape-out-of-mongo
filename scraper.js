e21502b51776bad711b0214b92e173b0bb2d4916
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-05-30 16:48:48 -0700

    Added starting logic for scraping website

---
+++ scraper.js
@@ -0,0 +1,36 @@
+var cheerio = require("cheerio");
+
+var axios = require("axios");
+
+console.log(
+  "***************************************\n" +
+    "Grabbing Data\n" +
+    "***************************************\n"
+);
+
+// Need to request the page via axios
+axios
+  .get("https://www.entrepreneur.com/topic/coding")
+  .then(function (response) {
+    let $ = cheerio.load(response.data);
+
+    let results = [];
+
+    $(".pl").each(function (index, element) {
+      console.group(index);
+
+      //Grabbing the headline
+      console.log($(element).find("h3 a").text());
+
+      //Grabbing the article Link, which is relative to the root url
+      //In this case it will be $BASE_URL/article/######
+      console.log($(element).find("a").attr("href"));
+
+      //Grabbign the short summary of the article
+      console.log($(element).find(".deck").text());
+
+      // For the images will need to remove the "&blur=50" attribute from the string
+      console.log($(element).find("img").attr("src"));
+      console.groupEnd();
+    });
+  });
