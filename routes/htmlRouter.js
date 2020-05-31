10cb2651202251e5b92b9883b0612d8a9ce5272c
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-05-30 21:25:58 -0700

    Added Styling for displaying the articles

--- routes/htmlRouter.js
+++ routes/htmlRouter.js
@@ -3,7 +3,7 @@ const scraper = require("../scraper.js");
 module.exports = function (app) {
   app.get("/", function (req, res) {
     scraper().then(function (results) {
-      console.log(results);
+      //   console.log(results);
       res.render("index", { article: results });
     });
   });
