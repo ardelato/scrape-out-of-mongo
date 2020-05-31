dec534f125e895996205f0bdca4d3edb7b7fbb1b
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-05-30 18:15:56 -0700

    Changed article url logic to include baseurl

--- scraper.js
+++ scraper.js
@@ -10,37 +10,22 @@ console.log(
 );
 
 // Need to request the page via axios
-axios
-  .get("https://www.entrepreneur.com/topic/coding")
-  .then(function (response) {
-    let $ = cheerio.load(response.data);
+const baseURL = "https://www.entrepreneur.com";
+axios.get(`${baseURL}/topic/coding`).then(function (response) {
+  let $ = cheerio.load(response.data);
 
-    let results = [];
+  let results = [];
 
-    $(".pl").each(function (index, element) {
-      //   console.group(index);
+  $(".pl").each(function (index, element) {
+    let aObj = {
+      headline: $(element).find("h3 a").text(),
+      link: baseURL + $(element).find("a").attr("href"),
+      summary: $(element).find(".deck").text(),
+      image: $(element).find("img").attr("src").replace("&blur=50", "")
+    };
 
-      //   //Grabbing the headline
-      //   console.log($(element).find("h3 a").text());
-
-      //   //Grabbing the article Link, which is relative to the root url
-      //   //In this case it will be $BASE_URL/article/######
-      //   console.log($(element).find("a").attr("href"));
-
-      //   //Grabbign the short summary of the article
-      //   console.log($(element).find(".deck").text());
-
-      //   // For the images will need to remove the "&blur=50" attribute from the string
-      //   console.log($(element).find("img").attr("src").replace("&blur=50", ""));
-
-      let aObj = {
-        headline: $(element).find("h3 a").text(),
-        link: $(element).find("a").attr("href"),
-        summary: $(element).find(".deck").text(),
-        image: $(element).find("img").attr("src").replace("&blur=50", "")
-      };
-      // Pass article object that will be destructured
-      createArticle(aObj);
-      //   console.groupEnd();
-    });
+    // Pass article object that will be destructured
+    createArticle(aObj);
+    //   console.groupEnd();
   });
+});
